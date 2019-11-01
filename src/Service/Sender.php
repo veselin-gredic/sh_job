fe0b5b82fb0afa1b02372be8ef72b48004576683
veselin gredic <veselin.gredic@gmail.com>
2019-11-01 23:49:43 +0100

    Decopling email sending

---
+++ src/Service/Sender.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gredicv
+ * Date: 11/1/19
+ * Time: 8:42 PM
+ */
+
+namespace App\Service;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
+
+
+final class Sender implements SenderInterface
+{
+    public function send(Email $mail): void
+    {
+        $transport = Transport::fromDsn($_ENV['MAILER_DSN']);
+        $mailer = new Mailer($transport);
+        $mailer->send($mail);
+    }
+}
